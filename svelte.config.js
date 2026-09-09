fb362dcc27620003228200a53ebe0da8345d761d
ndacom <158173896+ndacom@users.noreply.github.com>
2026-09-09 11:07:49 +0700

    fix

--- svelte.config.js
+++
@@ -1,8 +0,0 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('svelte').Config} */
-const config = {
-  preprocess: vitePreprocess()
-};
-
-export default config;
